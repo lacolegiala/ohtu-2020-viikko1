b0be73c776f373591c726b19e322e0d8c3386a99
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-11-08 21:39:12 +0200

    Fix curly brace

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -1,7 +1,6 @@
 package ohtu.ohtuvarasto;
 
-public class Varasto 
-{
+public class Varasto {
 
     // --- piilotettu tietorakenteen toteutus: ---
     private double tilavuus;  // paljonko varastoon mahtuu,  > 0
